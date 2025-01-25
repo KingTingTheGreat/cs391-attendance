ebac32c352ce849da524637da6c8a1054b2f2499
Jeffrey <kingtingthegreat@gmail.com>
2025-01-24 19:13:00 -0500

    logic moving

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { cookies } from "next/headers";
 import Link from "next/link";
 import Image from "next/image";
 import PresentButton from "@/components/present-button";
-import { formatDate } from "@/lib/attendanceList";
 
 export default async function Home() {
   const cookieStore = await cookies();
@@ -21,18 +20,12 @@ export default async function Home() {
     );
   }
 
-  const presentToday =
-    user.attendanceList &&
-    user.attendanceList.length > 0 &&
-    formatDate(user.attendanceList[user.attendanceList.length - 1].date) ===
-      formatDate(new Date());
-
   return (
     <div>
       <h1 className="text-4xl font-bold">CS391 Attendance</h1>
       <p>You are signed in</p>
       <p>Hi {user.name}!</p>
-      <PresentButton presentInput={presentToday} />
+      <PresentButton user={user} />
       <Image
         src={user.picture}
         alt="profile picture"
--- components/present-button.tsx
+++ components/present-button.tsx
@@ -2,30 +2,30 @@
 import markAsPresent from "@/lib/markAsPresent";
 import { useState } from "react";
 import Loading from "./loading";
+import { UserProps } from "@/types";
+import { formatDate } from "@/lib/attendanceList";
 
-export default function PresentButton({
-  presentInput,
-}: {
-  presentInput: boolean | undefined;
-}) {
-  const [present, setPresent] = useState(presentInput);
+export default function PresentButton({ user }: { user: UserProps }) {
+  const [present, setPresent] = useState(
+    user.attendanceList &&
+      user.attendanceList.length > 0 &&
+      formatDate(user.attendanceList[user.attendanceList.length - 1].date) ===
+        formatDate(new Date()),
+  );
 
   if (present === undefined) return <Loading />;
   if (present) {
-    return <div>marked present</div>;
+    return (
+      <div>
+        <p>You have been marked present!</p>
+      </div>
+    );
   }
 
   return (
     <div>
       <p>Were you in class today?</p>
-      <button
-        onClick={() =>
-          markAsPresent().then((res) => {
-            alert("marked as present" + res);
-            setPresent(presentInput || res);
-          })
-        }
-      >
+      <button onClick={() => markAsPresent().then((res) => setPresent(res))}>
         Yes
       </button>
       <button>No</button>
