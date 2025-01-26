73cdd66e75d2d0a7400326358133c33fe0e2147d
Jeffrey <kingtingthegreat@gmail.com>
2025-01-26 00:17:42 -0500

    styling

--- components/edit-role.tsx
+++ components/edit-role.tsx
@@ -1,52 +1,65 @@
 "use client";
 import { EditUserRole } from "@/lib/control/editUserRole";
 import { Role, UserProps } from "@/types";
+import { Button, MenuItem, Select } from "@mui/material";
 import { useState } from "react";
 
 export default function EditRole({ users }: { users: UserProps[] }) {
-  const [selectedEmail, setSelectedEmail] = useState<string | null>(null);
-  const [newRole, setNewRole] = useState<Role | null>(null);
+  const [selectedEmail, setSelectedEmail] = useState<string>("");
+  const [newRole, setNewRole] = useState<Role | string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
 
   return (
-    <div>
-      <h3>All User Emails</h3>
-      <select
+    <div className="p-2 m-2 w-fit">
+      <h3 className="text-2xl font-semibold text-center w-full">
+        Edit User Role
+      </h3>
+      <Select
         onChange={(e) => {
-          setSelectedEmail(e.target.value || null);
+          setSelectedEmail(e.target.value as string);
         }}
+        value={selectedEmail}
+        sx={{ minWidth: 150, width: "fit-content", margin: "0.25rem" }}
       >
-        <option></option>
+        <MenuItem value=""></MenuItem>
         {users.map((user) => (
-          <option key={user.email} value={user.email}>
+          <MenuItem key={user.email} value={user.email}>
             {user.email}
-          </option>
+          </MenuItem>
         ))}
-      </select>
-      <select onChange={(e) => setNewRole((e.target.value as Role) || null)}>
-        <option></option>
+      </Select>
+      <Select
+        onChange={(e) =>
+          setNewRole(e.target.value ? (e.target.value as Role) : "")
+        }
+        value={newRole}
+        sx={{ width: 100, margin: "0.25rem" }}
+      >
+        <MenuItem value=""></MenuItem>
         {Object.keys(Role).map(
           (role) =>
             role !== Role.admin && (
-              <option key={role} value={role}>
+              <MenuItem key={role} value={role}>
                 {role}
-              </option>
+              </MenuItem>
             ),
         )}
-      </select>
+      </Select>
 
-      <button
+      <Button
         disabled={!(selectedEmail && newRole)}
+        variant="contained"
+        sx={{ margin: "0.25rem" }}
         onClick={() => {
           if (selectedEmail && newRole) {
-            EditUserRole(selectedEmail, newRole).then((msg) =>
+            EditUserRole(selectedEmail, newRole as Role).then((msg) =>
               setErrorMessage(msg as string),
             );
           }
         }}
       >
         Edit Role
-      </button>
+      </Button>
       <p>{errorMessage}</p>
     </div>
   );
