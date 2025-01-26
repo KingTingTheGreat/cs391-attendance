fcd59b9119dc24649d49fc2664d37c923ab8c301
Jeffrey <kingtingthegreat@gmail.com>
2025-01-25 22:47:22 -0500

    fetch from db in prod

--- lib/allUsers.ts
+++ lib/allUsers.ts
@@ -1,26 +1,21 @@
 "use server";
 import getCollection, { USERS_COLLECTION } from "@/db";
-import { AttendanceProps, Role, UserProps } from "@/types";
+import { UserProps } from "@/types";
 import { mockStudents } from "./mockStudents";
 
 export async function allUsers(): Promise<UserProps[]> {
   if (process.env.ENVIRONMENT === "dev") {
     return mockStudents();
   }
-  // const usersCollection = await getCollection(USERS_COLLECTION);
-  //
-  // const users = await usersCollection.find().toArray();
 
-  const x = mockStudents();
-  // x.push(
-  //   ...users.map((user) => ({
-  //     name: user.name as string,
-  //     email: user.email as string,
-  //     picture: user.picture as string,
-  //     role: user.role as Role,
-  //     attendanceList: user.attendanceList as AttendanceProps[],
-  //   })),
-  // );
+  const usersCollection = await getCollection(USERS_COLLECTION);
+  const users = await usersCollection.find().toArray();
 
-  return x;
+  return users.map((user) => ({
+    name: user.name,
+    email: user.email,
+    picture: user.picture,
+    role: user.role,
+    attendanceList: user.attendanceList,
+  }));
 }
