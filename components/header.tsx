7b04681eda2f0e438fb47a3ad27bcc7f5060cb91
Jeffrey <kingtingthegreat@gmail.com>
2025-01-27 13:53:28 -0500

    styling

--- components/header.tsx
+++ components/header.tsx
@@ -5,12 +5,12 @@ const allowedRoles = [Role.staff, Role.admin];
 
 export default function Header({ role }: { role?: Role }) {
   return (
-    <header className="p-4 flex justify-center md:justify-between">
-      <Link href="/" className="text-4xl font-bold text-center">
+    <header className="p-4 flex flex-col lg:flex-row lg:justify-between items-center">
+      <Link href="/" className="text-3xl md:text-4xl font-bold text-center">
         CS391 Attendance
       </Link>
       {role && allowedRoles.includes(role) && (
-        <nav className="text-2xl font-semibold">
+        <nav className="text-lg md:text-2xl font-semibold">
           <Link href="/code" className="p-2 m-2">
             Today&apos;s Code
           </Link>
