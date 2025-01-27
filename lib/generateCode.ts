3a1599ed03e2219276b54bd7deee2a0b053f5982
Jeffrey <kingtingthegreat@gmail.com>
2025-01-27 00:59:02 -0500

    formatting

--- lib/generateCode.ts
+++ lib/generateCode.ts
@@ -2,8 +2,9 @@ import { createHash } from "crypto";
 import { formatDate } from "./util/format";
 
 export function generateCode() {
+  const secret = process.env.SECRET as string;
   return createHash("sha256")
-    .update((formatDate(new Date()) + process.env.SECRET) as string)
+    .update(formatDate(new Date()) + secret)
     .digest("hex")
     .slice(0, 8)
     .toUpperCase();
