c19ad9936d6acf0bec2c566db5f6bec780266ec5
Jeffrey <kingtingthegreat@gmail.com>
2025-01-29 18:50:52 -0500

    separate dev and prod cache

--- lib/cache/redis.ts
+++ lib/cache/redis.ts
@@ -1,21 +1,22 @@
 import { UserProps } from "@/types";
 import { Redis } from "@upstash/redis";
 import { parseUser } from "./parseUser";
+import { ENV } from "../env";
 
 const redis = Redis.fromEnv();
 
 export async function getFromCache(email: string): Promise<UserProps | null> {
-  const result = await redis.get(email);
+  const result = await redis.get(`${email}-${ENV}`);
   return parseUser(result as UserProps);
 }
 
 export async function setToCache(user: UserProps) {
   // expiry in 60 seconds * 5 = 5 minutes
-  await redis.set(user.email, user, { ex: 60 * 5 });
+  await redis.set(`${user.email}-${ENV}`, user, { ex: 60 * 5 });
 }
 
 export async function deleteFromCache(email: string) {
-  await redis.del(email);
+  await redis.del(`${email}-${ENV}`);
 }
 
 export async function clearCache() {
