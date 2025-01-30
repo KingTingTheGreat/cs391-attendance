3e9e973c65b73457190193e20eaa736f06a3dc86
Jeffrey <kingtingthegreat@gmail.com>
2025-01-29 19:06:25 -0500

    remove cache cookie references

--- lib/cookies/clearUserCookie.ts
+++ lib/cookies/clearUserCookie.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import { AUTH_COOKIE, CACHE_COOKIE } from "./cookies";
+import { AUTH_COOKIE } from "./cookies";
 
 export async function clearUserCookie(response: NextResponse) {
   response.cookies.delete(AUTH_COOKIE);
-  response.cookies.delete(CACHE_COOKIE);
 }
