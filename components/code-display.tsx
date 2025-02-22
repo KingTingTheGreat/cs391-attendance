16720dbf542ebb592a0b701e7e2e64ee8cbaa00d
Jeffrey <kingtingthegreat@gmail.com>
2025-02-22 14:34:52 -0500

    increase font size

--- components/code-display.tsx
+++ components/code-display.tsx
@@ -1,6 +1,6 @@
 export default function CodeDisplay({ code }: { code: string }) {
   return (
-    <div className="text-5xl font-bold tracking-wider bg-gray-200 p-4 m-2 rounded-lg">
+    <div className="text-6xl font-bold tracking-wider bg-gray-200 p-4 m-2 rounded-lg">
       {code}
     </div>
   );
