0beff67e648e254d6672d58e1971f0227f95d331
Jeffrey <kingtingthegreat@gmail.com>
2025-03-10 16:35:17 -0400

    refactor

---
+++ components/student/AttendanceForm.tsx
@@ -0,0 +1,87 @@
+import { Dispatch, SetStateAction, useState, useTransition } from "react";
+import markAsPresent from "@/lib/student/markAsPresent";
+import { Button, TextField } from "@mui/material";
+import { Class, UserProps } from "@/types";
+import { formatDate } from "@/lib/util/format";
+import { addToAttendanceList } from "@/lib/util/addToAttendanceList";
+
+export default function AttendanceForm({
+  user,
+  setUser,
+}: {
+  user: UserProps;
+  setUser: Dispatch<SetStateAction<UserProps>>;
+}) {
+  const [code, setCode] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const [errorMessage, setErrorMessage] = useState("");
+  const today = formatDate(new Date());
+  const [present, setPresent] = useState(
+    user.attendanceList.some((att) => formatDate(att.date) === today),
+  );
+
+  const processPresentRes = (errMsg: string | null) => {
+    if (errMsg === null) {
+      setUser({
+        ...user,
+        attendanceList: addToAttendanceList(user.attendanceList, {
+          class: Class.lecture,
+          date: new Date(),
+        }),
+      });
+      setPresent(true);
+      setErrorMessage("");
+    } else {
+      setErrorMessage(errMsg);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      {!present ? (
+        <form
+          action={() => {
+            startTransition(() => {
+              if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((pos) => {
+                  markAsPresent(
+                    code,
+                    pos.coords.longitude,
+                    pos.coords.latitude,
+                  ).then(processPresentRes);
+                });
+              } else {
+                markAsPresent(code).then(processPresentRes);
+              }
+            });
+          }}
+          className="flex flex-col w-72"
+        >
+          <TextField
+            type="text"
+            value={code}
+            placeholder="Today's Code"
+            variant="outlined"
+            sx={{ margin: "0.1rem" }}
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={isPending}
+            sx={{ margin: "0.1rem" }}
+          >
+            {isPending
+              ? "Marking you as present..."
+              : "Click to indicate you're in class"}
+          </Button>
+        </form>
+      ) : (
+        <div className="text-center p-4 bg-green-100 text-green-700 rounded-md">
+          You have been marked present for today!
+        </div>
+      )}
+      <p className="p-2 text-lg text-[#F00]">{errorMessage}</p>
+    </div>
+  );
+}
--- components/student/student-profile.tsx
+++ components/student/student-profile.tsx
@@ -1,13 +1,10 @@
 "use client";
-import markAsPresent from "@/lib/student/markAsPresent";
 import Image from "next/image";
-import { useState, useTransition } from "react";
-import { Class, UserProps } from "@/types";
-import { formatDate } from "@/lib/util/format";
-import { Button, TextField } from "@mui/material";
-import { addToAttendanceList } from "@/lib/util/addToAttendanceList";
+import { useState } from "react";
+import { UserProps } from "@/types";
 import "react-day-picker/style.css";
 import AttendanceCalendar from "./AttendanceCalendar";
+import AttendanceForm from "./AttendanceForm";
 
 export default function StudentProfile({
   userInput,
@@ -15,73 +12,6 @@ export default function StudentProfile({
   userInput: UserProps;
 }) {
   const [user, setUser] = useState(userInput);
-  const [code, setCode] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const formatToday = formatDate(new Date());
-  const [present, setPresent] = useState(
-    user.attendanceList.some((att) => formatDate(att.date) === formatToday),
-  );
-  const [isPending, startTransition] = useTransition();
-
-  const processPresentRes = (errMsg: string | null) => {
-    if (errMsg === null) {
-      setUser({
-        ...user,
-        attendanceList: addToAttendanceList(user.attendanceList, {
-          class: Class.lecture,
-          date: new Date(),
-        }),
-      });
-      setPresent(true);
-      setErrorMessage("");
-    } else {
-      setErrorMessage(errMsg);
-    }
-  };
-
-  const interactive = !present ? (
-    <form
-      action={() => {
-        startTransition(() => {
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((pos) => {
-              markAsPresent(
-                code,
-                pos.coords.longitude,
-                pos.coords.latitude,
-              ).then(processPresentRes);
-            });
-          } else {
-            markAsPresent(code).then(processPresentRes);
-          }
-        });
-      }}
-      className="flex flex-col w-72"
-    >
-      <TextField
-        type="text"
-        value={code}
-        placeholder="Today's Code"
-        variant="outlined"
-        sx={{ margin: "0.1rem" }}
-        onChange={(e) => setCode(e.target.value)}
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        disabled={isPending}
-        sx={{ margin: "0.1rem" }}
-      >
-        {isPending
-          ? "Marking you as present..."
-          : "Click to indicate you're in class"}
-      </Button>
-    </form>
-  ) : (
-    <div className="text-center p-4 bg-green-100 text-green-700 rounded-md">
-      You have been marked present for today!
-    </div>
-  );
 
   return (
     <div>
@@ -97,8 +27,7 @@ export default function StudentProfile({
           className="rounded-full"
         />
       </div>
-      <div className="flex justify-center">{interactive}</div>
-      <p className="p-2 text-lg text-[#F00]">{errorMessage}</p>
+      <AttendanceForm user={user} setUser={setUser} />
       <AttendanceCalendar user={user} />
     </div>
   );
