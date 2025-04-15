6d66afa98fbe3e183a8cd52434ccc556841875c9
Jeffrey <kingtingthegreat@gmail.com>
2025-04-15 11:06:37 -0400

    display multiple class types of presence for today

--- components/student/AttendanceForm.tsx
+++ components/student/AttendanceForm.tsx
@@ -4,14 +4,13 @@ import { Button, TextField } from "@mui/material";
 import { formatDate } from "@/lib/util/format";
 import { addToAttendanceList } from "@/lib/util/addToAttendanceList";
 import { useStudentContext } from "./StudentContext";
+import PresentMessage from "./PresentMessage";
+import { Class } from "@/types";
 
 export default function AttendanceForm() {
   const { user, setUser } = useStudentContext();
   const [code, setCode] = useState("");
   const today = formatDate(new Date());
-  const [present, setPresent] = useState(
-    user.attendanceList.some((att) => formatDate(att.date) === today),
-  );
   const [error, submitAction, isPending] = useActionState(async () => {
     try {
       const newAtt = await markAsPresent(code);
@@ -19,7 +18,6 @@ export default function AttendanceForm() {
         ...user,
         attendanceList: addToAttendanceList(user.attendanceList, newAtt),
       });
-      setPresent(true);
 
       return null;
     } catch (e) {
@@ -29,33 +27,41 @@ export default function AttendanceForm() {
 
   return (
     <div className="flex flex-col items-center">
-      {!present ? (
-        <form action={submitAction} className="flex flex-col w-72">
-          <TextField
-            type="text"
-            value={code}
-            placeholder="Today's Code"
-            variant="outlined"
-            sx={{ margin: "0.1rem" }}
-            onChange={(e) => setCode(e.target.value)}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            disabled={isPending}
-            sx={{ margin: "0.1rem" }}
-          >
-            {isPending
-              ? "Marking you as present..."
-              : "Click to indicate you're in class"}
-          </Button>
-        </form>
-      ) : (
-        <div className="text-center p-4 bg-green-100 text-green-700 rounded-md">
-          You have been marked present for today!
-        </div>
-      )}
-      <p className="p-2 text-lg text-[#F00]">{error && error.message}</p>
+      <form action={submitAction} className="flex flex-col w-72">
+        <TextField
+          type="text"
+          value={code}
+          placeholder="Today's Code"
+          variant="outlined"
+          sx={{ margin: "0.1rem" }}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          disabled={isPending}
+          sx={{ margin: "0.1rem" }}
+        >
+          {isPending
+            ? "Marking you as present..."
+            : "Click to indicate you're in class"}
+        </Button>
+      </form>
+      <div className="grid grid-cols-1 gap-0.5 p-2">
+        {Object.keys(Class).map((classType) => {
+          if (
+            user.attendanceList.some(
+              (att) =>
+                formatDate(att.date) === today && att.class === classType,
+            )
+          ) {
+            return (
+              <PresentMessage classType={classType as Class} key={classType} />
+            );
+          }
+        })}
+      </div>
+      <p className="p-0.5 text-lg text-[#F00]">{error && error.message}</p>
     </div>
   );
 }
