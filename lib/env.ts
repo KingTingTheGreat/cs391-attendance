89cd27d6e5903c748ceec2d23c4fbaa205191bef
Jeffrey <kingtingthegreat@gmail.com>
2025-05-06 12:53:07 -0400

    no redis option

--- lib/env.ts
+++ lib/env.ts
@@ -4,3 +4,4 @@ export const ENV = process.env.ENVIRONMENT;
 export const DEFAULT_ROLE = process.env.DEFAULT_ROLE as Role;
 export const MOCK = process.env.MOCK === "true";
 export const ENABLE_SIGN_ON = process.env.ENABLE_SIGN_ON === "true";
+export const NO_REDIS = process.env.NEXT_PUBLIC_NO_REDIS === "true";
