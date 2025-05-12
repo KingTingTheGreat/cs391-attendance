e4557e873b1cfb7bcb1d969f331a8470f7b7cc43
Jeffrey <kingtingthegreat@gmail.com>
2025-05-12 10:52:09 -0400

    new environment variables

--- lib/env.ts
+++ lib/env.ts
@@ -5,3 +5,19 @@ export const DEFAULT_ROLE = process.env.DEFAULT_ROLE as Role;
 export const MOCK = process.env.MOCK === "true";
 export const ENABLE_SIGN_ON = process.env.ENABLE_SIGN_ON === "true";
 export const NO_REDIS = process.env.NEXT_PUBLIC_NO_REDIS === "true";
+export const NUM_CODES = (() => {
+  const numCodes = Number(process.env.NUM_CODES);
+  if (isNaN(numCodes) || numCodes < 0) {
+    // default to 3
+    return 3;
+  }
+  return numCodes;
+})();
+export const GRACE_PERIOD = () => {
+  const gracePeriod = Number(process.env.GRACE_PERIOD);
+  if (isNaN(gracePeriod) || gracePeriod < 0) {
+    // default to 0 ms
+    return 0;
+  }
+  return gracePeriod;
+};
