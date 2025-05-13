d9d0fce49bb53eb6eccf22afed2c0e5abb1b9ca6
Jeffrey <kingtingthegreat@gmail.com>
2025-05-13 16:28:31 -0400

    environment variables

--- lib/env.ts
+++ lib/env.ts
@@ -4,7 +4,6 @@ export const ENV = process.env.ENVIRONMENT;
 export const DEFAULT_ROLE = process.env.DEFAULT_ROLE as Role;
 export const MOCK = process.env.MOCK === "true";
 export const ENABLE_SIGN_ON = process.env.ENABLE_SIGN_ON === "true";
-export const NO_REDIS = process.env.NEXT_PUBLIC_NO_REDIS === "true";
 export const NUM_CODES = (() => {
   const numCodes = Number(process.env.NUM_CODES);
   if (isNaN(numCodes) || numCodes < 0) {
@@ -13,6 +12,8 @@ export const NUM_CODES = (() => {
   }
   return numCodes;
 })();
+// acceptable window in both direction (before and after)
+// in milliseconds
 export const GRACE_PERIOD = (() => {
   const gracePeriod = Number(process.env.GRACE_PERIOD);
   if (isNaN(gracePeriod) || gracePeriod < 0) {
@@ -20,4 +21,22 @@ export const GRACE_PERIOD = (() => {
     return 0;
   }
   return gracePeriod;
+})();
+// temporary code ttl in seconds
+export const TTL = (() => {
+  const ttl = Number(process.env.TTL);
+  if (isNaN(ttl) || ttl < 0) {
+    // default to 10 seconds
+    return 10 * 1000;
+  }
+  return ttl * 1000;
+})();
+// how often the temporary code display refreshes in milliseconds
+export const INTERVAL_LENGTH = (() => {
+  const ttl = Number(process.env.INTERVAL_LENGTH);
+  if (isNaN(ttl) || ttl < 0) {
+    // default to 250 milliseconds
+    return 250;
+  }
+  return ttl;
 })();
