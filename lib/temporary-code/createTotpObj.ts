24b82ca6a3961966c1f23e1551f66dde52850166
Jeffrey <kingtingthegreat@gmail.com>
2025-05-15 02:22:38 -0400

    allow manual secret specification for calling from frontend

--- lib/temporary-code/createTotpObj.ts
+++ lib/temporary-code/createTotpObj.ts
@@ -2,24 +2,24 @@ import { TOTP } from "otpauth";
 import { getInputTempCodeKey, getScanTempCodeKey } from "./getTempCodeKey";
 import { Class } from "@/types";
 
-const SCAN_TEMP_CODE_LENGTH = 12;
-export const SCAN_TEMP_CODE_PERIOD = 5;
+const INPUT_TEMP_CODE_LENGTH = 6;
+export const INPUT_TEMP_CODE_PERIOD = 15;
 
-export function createScanTotp(classType: Class) {
+export function createInputTotp(classType: Class, secret?: string) {
   return new TOTP({
-    secret: getScanTempCodeKey(classType),
-    digits: SCAN_TEMP_CODE_LENGTH,
-    period: SCAN_TEMP_CODE_PERIOD,
+    secret: secret || getInputTempCodeKey(classType),
+    digits: INPUT_TEMP_CODE_LENGTH,
+    period: INPUT_TEMP_CODE_PERIOD,
   });
 }
 
-const INPUT_TEMP_CODE_LENGTH = 6;
-export const INPUT_TEMP_CODE_PERIOD = 5;
+const SCAN_TEMP_CODE_LENGTH = 12;
+export const SCAN_TEMP_CODE_PERIOD = 5;
 
-export function createInputTotp(classType: Class) {
+export function createScanTotp(classType: Class, secret?: string) {
   return new TOTP({
-    secret: getInputTempCodeKey(classType),
-    digits: INPUT_TEMP_CODE_LENGTH,
-    period: INPUT_TEMP_CODE_PERIOD,
+    secret: secret || getScanTempCodeKey(classType),
+    digits: SCAN_TEMP_CODE_LENGTH,
+    period: SCAN_TEMP_CODE_PERIOD,
   });
 }
