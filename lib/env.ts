2f452e8abd2fb7013e050d7190a600afaf996d08
Jeffrey <kingtingthegreat@gmail.com>
2025-05-15 13:32:13 -0400

    change default

--- lib/env.ts
+++ lib/env.ts
@@ -34,7 +34,7 @@ export const SCAN_TEMP_CODE_LENGTH = (() => {
     process.env.NEXT_PUBLIC_SCAN_TEMP_CODE_LENGTH,
   );
   if (isNaN(scanTempCodeLength) || scanTempCodeLength < 0) {
-    return 20;
+    return 10;
   }
   return scanTempCodeLength;
 })();
