d64c0c895fc2af1e57c4f27df1977b7bf1f1ee41
Jeffrey <kingtingthegreat@gmail.com>
2025-05-15 02:18:29 -0400

    base32 encode

---
+++ lib/b32.ts
@@ -0,0 +1,22 @@
+export function base32Encode(input: Buffer<ArrayBufferLike>) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+  let bits = 0;
+  let value = 0;
+  let output = "";
+
+  for (let i = 0; i < input.length; i++) {
+    value = (value << 8) | input[i];
+    bits += 8;
+
+    while (bits >= 5) {
+      output += alphabet[(value >>> (bits - 5)) & 31];
+      bits -= 5;
+    }
+  }
+
+  if (bits > 0) {
+    output += alphabet[(value << (5 - bits)) & 31];
+  }
+
+  return output;
+}
