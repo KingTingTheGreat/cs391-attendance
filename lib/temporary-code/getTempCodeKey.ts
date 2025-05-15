021687a434524a51385efdc688f4ce83658407c0
Jeffrey <kingtingthegreat@gmail.com>
2025-05-15 02:20:43 -0400

    b32 encode

--- lib/temporary-code/getTempCodeKey.ts
+++ lib/temporary-code/getTempCodeKey.ts
@@ -1,28 +1,31 @@
 import { createHash } from "crypto";
 import { formatDate } from "../util/format";
 import { Class } from "@/types";
+import { base32Encode } from "../b32";
 
-const TOKEN_KEY_LENGTH = 16;
-
-export const SCAN_KEY_CONSTANT = "scan-temporary-code-key";
 export const INPUT_KEY_CONSTANT = "input-temporary-code-key";
-
-export function getScanTempCodeKey(classType: Class) {
-  const secret = process.env.SECRET as string;
-  if (!secret) throw new Error("todayCode() must be called on the server");
-  return createHash("sha256")
-    .update(secret + formatDate(new Date()) + classType + SCAN_KEY_CONSTANT)
-    .digest("base64url")
-    .slice(0, TOKEN_KEY_LENGTH)
-    .toUpperCase();
-}
 
 export function getInputTempCodeKey(classType: Class) {
   const secret = process.env.SECRET as string;
-  if (!secret) throw new Error("todayCode() must be called on the server");
-  return createHash("sha256")
-    .update(secret + formatDate(new Date()) + classType + INPUT_KEY_CONSTANT)
-    .digest("base64url")
-    .slice(0, TOKEN_KEY_LENGTH)
-    .toUpperCase();
+  if (!secret)
+    throw new Error("getInputTempCodeKey() must be called on the server");
+  console.log("returning input temp code key");
+  return base32Encode(
+    createHash("sha256")
+      .update(secret + formatDate(new Date()) + classType + INPUT_KEY_CONSTANT)
+      .digest(),
+  );
+}
+
+export const SCAN_KEY_CONSTANT = "scan-temporary-code-key";
+
+export function getScanTempCodeKey(classType: Class) {
+  const secret = process.env.SECRET as string;
+  if (!secret)
+    throw new Error("getScanTempCodeKey() must be called on the server");
+  return base32Encode(
+    createHash("sha256")
+      .update(secret + formatDate(new Date()) + classType + SCAN_KEY_CONSTANT)
+      .digest(),
+  );
 }
