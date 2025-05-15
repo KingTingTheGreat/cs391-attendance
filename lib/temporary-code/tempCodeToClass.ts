aae554905b1437d1e3170f572ce04a7de5541cca
Jeffrey <kingtingthegreat@gmail.com>
2025-05-15 11:38:36 -0400

    convert code to digits

--- lib/temporary-code/tempCodeToClass.ts
+++ lib/temporary-code/tempCodeToClass.ts
@@ -1,14 +1,20 @@
 import { Class } from "@/types";
 import { createInputTotp, createScanTotp } from "./createTotpObj";
 import { WINDOW_SIZE } from "../env";
+import { base32CodeToDigits } from "../b32";
 
 export default function tempCodeToClass(
   code: string,
   onlyScan?: boolean,
 ): Class | null {
+  const digits = base32CodeToDigits(code);
+  if (digits === null) {
+    return null;
+  }
+
   for (const classType of Object.keys(Class)) {
     const scanTOTP = createScanTotp(classType as Class);
-    if (scanTOTP.validate({ token: code, window: WINDOW_SIZE }) !== null) {
+    if (scanTOTP.validate({ token: digits, window: WINDOW_SIZE }) !== null) {
       return classType as Class;
     }
     if (onlyScan) {
@@ -16,7 +22,7 @@ export default function tempCodeToClass(
     }
 
     const inputTOTP = createInputTotp(classType as Class);
-    if (inputTOTP.validate({ token: code, window: WINDOW_SIZE }) !== null) {
+    if (inputTOTP.validate({ token: digits, window: WINDOW_SIZE }) !== null) {
       return classType as Class;
     }
   }
